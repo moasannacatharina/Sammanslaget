81f366d35dfe01dee9ec073497abf980b7c1b02a
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-08-24 16:07:14 +0200

    Added about-page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,60 @@
+import "./about.css";
+import image from "../../assets/images/yrgo.png";
+
+import Card from "../../components/Card";
+
 const About = () => {
-  return <h1>About us</h1>;
+  return (
+    <>
+      <header>Vi som skapat "Utelämnade Mika"</header>
+      <div className="grid">
+        <Card
+          title="Lena Last UX-designer"
+          img="heheh"
+          info="Jag är en trevlig prick"
+          email="mail@gmail.com"
+          color="yellow"
+        />
+        <Card
+          title="Daniel Bradshaw Manusförfattare"
+          img="heheh"
+          info="HEJ!"
+          email="mail@gmail.com"
+          color="pink"
+        />
+        <Card
+          title="Oscar Elmgren Game Artist"
+          img="heheh"
+          info="HEJ!"
+          email="mail@gmail.com"
+          color="green"
+        />
+        <Card
+          title="Réka Madarász Webbutvecklare"
+          img="heheh"
+          info="HEJ!"
+          email="mail@gmail.com"
+          color="purple"
+        />
+        <Card
+          title="Moa Berg Webbutvecklare"
+          img="heheh"
+          info="HEJ!"
+          email="mail@gmail.com"
+          color="blue"
+        />
+        <Card color="hotpink" />
+      </div>
+      <footer>
+        <img src={image} alt="yrgo" />
+        <p>
+          YRGO yh-utbildning är vår gemensamma nämnare. Gå in på yrgo.se för att
+          få veta mer om våra utbildningar!
+        </p>
+        <a href="https://yrgo.se">Go! →</a>
+      </footer>
+    </>
+  );
 };
 
 export default About;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,9 +8,9 @@ const Home = () => {
       <div className="book">
         <HTMLFlipBook width={500} height={700}>
           <Page number="1" header="Start"></Page>
-          <Page number="2" header="hehe" even="true"></Page>
+          <Page number="2" header="hehe" even="even"></Page>
           <Page number="3" header="snart slut"></Page>
-          <Page number="4" header="the END" even="true"></Page>
+          <Page number="4" header="the END" even="even"></Page>
         </HTMLFlipBook>
       </div>
     </>
