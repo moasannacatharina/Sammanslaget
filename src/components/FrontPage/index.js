cda1e3269d695f4befb1ad2f9ae3710d3a25a31d
Reka Madarasz <mreka91@gmail.com>
2021-08-24 15:01:49 +0200

    added front page

---
+++ src/components/FrontPage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+const FrontPage = React.forwardRef((props, ref) => {
+  return (
+    <div className="page page-cover" ref={ref} data-density="hard">
+      <div className="page-content">
+        <h2>{props.children}</h2>
+      </div>
+    </div>
+  );
+});
+
+export default FrontPage;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import HTMLFlipBook from "react-pageflip";
-import Page from "../../components/Page";
+import Page from "../../components/page/index.js";
+import FrontPage from "../../components/FrontPage/index.js";
 import "./home.css";
 
 const Home = () => {
   return (
     <>
       <div className="book">
-        <HTMLFlipBook width={500} height={700}>
+        <HTMLFlipBook width={500} height={700} showCover={true}>
+          <FrontPage>The book's title</FrontPage>
           <Page number="1" header="Start"></Page>
           <Page number="2" header="hehe" even="true"></Page>
           <Page number="3" header="snart slut"></Page>
