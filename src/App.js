fc5d92826c7b35f2dc1842056a9878cf242895cf
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-08-24 11:24:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import HTMLFlipBook from "react-pageflip";
+import Page from "./components/page";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HTMLFlipBook width={300} height={500}>
+        <Page number="1" header="Start">
+          HELLO
+        </Page>
+        <Page number="2" header="hehe"></Page>
+        <Page number="3" header="snart slut"></Page>
+        <Page number="4" header="the END"></Page>
+      </HTMLFlipBook>
     </div>
   );
 }
