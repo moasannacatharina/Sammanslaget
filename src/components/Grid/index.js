224fad2d71008bc0086a56cae9a9c89b5a22b28d
Reka Madarasz <mreka91@gmail.com>
2021-08-25 08:03:33 +0200

    card

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -9,35 +9,35 @@ const Grid = () => {
       <Card
         title="Lena Last UX-designer"
         img={image}
-        info="Jag är en trevlig prick"
+        info="Ethical squid yuccie, fingerstache farm-to-table swag 90's green juice jean shorts pug gochujang. Tilde snackwave selvage echo park, lomo offal kombucha 8-bit listicle pickled fanny pack pork belly normcore sartorial waistcoat. "
         email="mail@gmail.com"
         color="#FFF0C3"
       />
       <Card
         title="Daniel Bradshaw Manusförfattare"
         img={image}
-        info="HEJ!"
+        info="Ethical squid yuccie, fingerstache farm-to-table swag 90's green juice jean shorts pug gochujang. Tilde snackwave selvage echo park, lomo offal kombucha 8-bit listicle pickled fanny pack pork belly normcore sartorial waistcoat. "
         email="mail@gmail.com"
         color="#FCD5CE"
       />
       <Card
         title="Oscar Elmgren Game Artist"
         img={image}
-        info="HEJ!"
+        info="Ethical squid yuccie, fingerstache farm-to-table swag 90's green juice jean shorts pug gochujang. Tilde snackwave selvage echo park, lomo offal kombucha 8-bit listicle pickled fanny pack pork belly normcore sartorial waistcoat. "
         email="mail@gmail.com"
         color="#D8E2DC"
       />
       <Card
         title="Réka Madarász Webbutvecklare"
         img={image}
-        info="HEJ!"
+        info="Ethical squid yuccie, fingerstache farm-to-table swag 90's green juice jean shorts pug gochujang. Tilde snackwave selvage echo park, lomo offal kombucha 8-bit listicle pickled fanny pack pork belly normcore sartorial waistcoat. "
         email="mail@gmail.com"
         color="#CDB4DB"
       />
       <Card
         title="Moa Berg Webbutvecklare"
         img={image}
-        info="HEJ!"
+        info="Ethical squid yuccie, fingerstache farm-to-table swag 90's green juice jean shorts pug gochujang. Tilde snackwave selvage echo park, lomo offal kombucha 8-bit listicle pickled fanny pack pork belly normcore sartorial waistcoat. "
         email="mail@gmail.com"
         color="#CCDBFD"
       />
