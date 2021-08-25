061b2bcedcd85d3f91db25b6913cd14e0651e0fd
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-08-25 10:51:37 +0200

    Added another book

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,54 +1,40 @@
+import { useCallback } from "react";
+import HTMLFlipBook from "react-pageflip";
+import Page from "../Page";
+import FrontPage from "../FrontPage/";
+
 const Book = () => {
+  const onFlip = useCallback((e) => {
+    console.log("Current page: " + e.data);
+  }, []);
+
   return (
-    <div className="closed-book">
-      <svg
-        width="485"
-        height="399"
-        viewBox="0 0 485 399"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M169.366 310.451L1.83592 55.46L316.634 0.548988L484.164 255.54L169.366 310.451Z"
-          stroke="black"
-        />
-        <line
-          x1="0.499976"
-          y1="60.9951"
-          x2="1.42155"
-          y2="154.996"
-          stroke="black"
-        />
-        <line
-          x1="0.423901"
-          y1="154.735"
-          x2="152.424"
-          y2="397.735"
-          stroke="black"
-        />
-        <line
-          x1="151.916"
-          y1="397.507"
-          x2="473.916"
-          y2="342.507"
-          stroke="black"
-        />
-        <line
-          x1="473.504"
-          y1="342.937"
-          x2="484.504"
-          y2="255.937"
-          stroke="black"
-        />
-        <line
-          x1="169.491"
-          y1="311.096"
-          x2="152.491"
-          y2="398.096"
-          stroke="black"
-        />
-      </svg>
-    </div>
+    <HTMLFlipBook
+      width={500}
+      height={700}
+      minWidth={315}
+      maxWidth={500}
+      minHeight={400}
+      maxHeight={700}
+      maxShadowOpacity={0.5}
+      showCover={true}
+      onFlip={onFlip}
+    >
+      <FrontPage>Mika</FrontPage>
+      <Page
+        number="1"
+        // header="Start"
+        children="Det här är berättelsen om ett ungt regnmoln, ett moln med framtidsutsikter och hopp om livet. Det är också berättelsen om hur det ibland kan vara tufft att ta motgångar och hur tufft det är att resa sig upp igen på egen hand och om hur en knuff i rätt rikning kan vara helt avgörande. Det här är berättelsen som vi valt att kalla “Utlämande Mika”."
+      ></Page>
+      <Page number="2" header="Akt 1" even="even"></Page>
+      <Page number="3" header="Akt 1"></Page>
+      <Page number="4" header="Akt 2" even="even"></Page>
+      <Page number="5" header="Akt 2"></Page>
+      <Page number="6" header="Akt 3" even="even"></Page>
+      <Page number="7" header="Akt 3"></Page>
+      <Page number="8" header="The End" even="even"></Page>
+      <FrontPage></FrontPage>
+    </HTMLFlipBook>
   );
 };
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,30 @@
-import React, { useCallback } from "react";
-import HTMLFlipBook from "react-pageflip";
-import Page from "../../components/page/index.js";
-import FrontPage from "../../components/FrontPage/index.js";
+import React, { useState } from "react";
+// import HTMLFlipBook from "react-pageflip";
+// import Page from "../../components/Page/index.js";
+// import FrontPage from "../../components/FrontPage/index.js";
+import Book from "../../components/Book";
+import ClosedBook from "../../components/ClosedBook";
 import "./home.css";
 // import sound from "../../assets/sound/Simple-Book-Page-Turn-www.fesliyanstudios.com-www.fesliyanstudios.com.mp3";
 
 const Home = () => {
-  const onFlip = useCallback((e) => {
-    console.log("Current page: " + e.data);
-  }, []);
+  const [visible, setVisible] = useState("");
+  const [invisible, setInvisible] = useState("");
+
+  console.log(visible);
   return (
-    <>
-      <div className="book">
-        <HTMLFlipBook
-          width={500}
-          height={700}
-          minWidth={315}
-          maxWidth={500}
-          minHeight={400}
-          maxHeight={700}
-          maxShadowOpacity={0.5}
-          showCover={true}
-          onFlip={onFlip}
-        >
-          <FrontPage>Mika</FrontPage>
-          <Page
-            number="1"
-            // header="Start"
-            children="Det här är berättelsen om ett ungt regnmoln, ett moln med framtidsutsikter och hopp om livet. Det är också berättelsen om hur det ibland kan vara tufft att ta motgångar och hur tufft det är att resa sig upp igen på egen hand och om hur en knuff i rätt rikning kan vara helt avgörande. Det här är berättelsen som vi valt att kalla “Utlämande Mika”."
-          ></Page>
-          <Page number="2" header="Akt 1" even="even"></Page>
-          <Page number="3" header="Akt 1"></Page>
-          <Page number="4" header="Akt 2" even="even"></Page>
-          <Page number="5" header="Akt 2"></Page>
-          <Page number="6" header="Akt 3" even="even"></Page>
-          <Page number="7" header="Akt 3"></Page>
-          <Page number="8" header="The End" even="even"></Page>
-          <FrontPage></FrontPage>
-        </HTMLFlipBook>
+    <div>
+      <ClosedBook
+        handleClick={() => {
+          setVisible("true");
+          setInvisible("false");
+        }}
+        visible={visible}
+      />
+      <div className={`book ${invisible}`}>
+        <Book />
       </div>
-    </>
+    </div>
   );
 };
 
